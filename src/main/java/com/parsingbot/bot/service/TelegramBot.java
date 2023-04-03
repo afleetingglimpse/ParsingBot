2e1d47f102a40d464020e2e47a240b61e274ece1
Kirill <kirill.ustimencko@gmail.com>
2023-04-03 09:43:01 +0300

    Added my Parser to project dependencies. Modified onUpdateReceived, added handleCommand to process input command

--- src/main/java/com/parsingbot/bot/service/TelegramBot.java
+++ src/main/java/com/parsingbot/bot/service/TelegramBot.java
@@ -2,18 +2,23 @@ package com.parsingbot.bot.service;
 
 import com.parsingbot.bot.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.parser.Parser;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+
 @Slf4j // logging
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
     private final BotConfig config;
-
 
     public TelegramBot(BotConfig config) {
         this.config = config;
@@ -27,15 +32,55 @@ public class TelegramBot extends TelegramLongPollingBot {
      */
     @Override
     public void onUpdateReceived(Update update) {
+        handleCommand(update);
+    }
+
+    private void handleCommand(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
             String messageText = update.getMessage().getText();
             log.info(String.format("Received message %s from user %s %s %s",
-                                    messageText,
-                                    update.getMessage().getChat().getFirstName(),
-                                    update.getMessage().getChat().getLastName(),
-                                    update.getMessage().getChat().getUserName()));
+                    messageText,
+                    update.getMessage().getChat().getFirstName(),
+                    update.getMessage().getChat().getLastName(),
+                    update.getMessage().getChat().getUserName()));
             long chatId = update.getMessage().getChatId();
-            sendMessageTest(chatId, "U said " + messageText);
+
+            switch (messageText) {
+                case "/start": // убрать константы во внешний файл
+                    handleStartCommand(update);
+                    sendMessageTest(chatId, "No respond to /start command yet");
+                    break;
+
+                case "/hh":
+                    handleHHcommand(update);
+
+            }
+        }
+    }
+
+    // Сделать норм обработку команды
+    private void handleStartCommand(Update update) {
+    }
+
+    // Сделать норм обработку команды
+    private void handleHHcommand(Update update) {
+        long chatId = update.getMessage().getChatId();
+        try {
+            Parser parser = new Parser();
+            Map<String, Map<String, String>> result = parser.parse();
+            parser.saveResult(result, new File("D:/Study/Programming/Java/ParsingBot/target/temp.txt"));
+
+            for (String vacancy : result.keySet()) {
+                //sendMessageTest(chatId, vacancy);
+                Map<String, String> subMap = result.get(vacancy);
+                for (String attr : subMap.keySet()) {
+                    //sendMessageTest(chatId, attr);
+                    if (attr.equals("link"))
+                        sendMessageTest(chatId, subMap.get(attr));
+                }
+            }
+        } catch (IOException e) {
+            log.error("Failed to initialise parser. Process aborted");
         }
     }
 
