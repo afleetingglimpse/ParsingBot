d25f8af8fba8c02d322c07255687e9b04bc49a66
Kirill <kirill.ustimencko@gmail.com>
2023-04-09 15:57:59 +0300

    minor

--- src/main/java/com/parsingbot/bot/BotApplication.java
+++ src/main/java/com/parsingbot/bot/BotApplication.java
@@ -1,6 +1,5 @@
 package com.parsingbot.bot;
 
-import com.parsingbot.bot.config.BotConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BotApplication {
 
 	public static void main(String[] args) {
-		BotConfig a = new BotConfig();
 		SpringApplication.run(BotApplication.class, args);
 	}
 }
