fd594a36f25afab0174e69f214a77a68ee0cba38
Kirill <kirill.ustimencko@gmail.com>
2023-04-15 16:56:34 +0300

    fixed

--- src/main/java/com/parsingbot/bot/entities/Vacancy.java
+++ src/main/java/com/parsingbot/bot/entities/Vacancy.java
@@ -1,9 +1,4 @@
 package com.parsingbot.bot.entities;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Data;
 
 import java.util.Map;
